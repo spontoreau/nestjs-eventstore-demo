e1babaeba0830b465e745e650c7f4725ac20c7f4
spontoreau <pontoreau.sylvain@gmail.com>
2019-09-06 14:05:26 +0200

    Rename

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './account/account.module';
 import { CqrsModule } from '@nestjs/cqrs';
-import { EventStoreModule } from './store/event-store.module';
+import { EventStoreModule } from './event-store/event-store.module';
 
 @Module({
   imports: [CqrsModule, EventStoreModule, AccountModule],
